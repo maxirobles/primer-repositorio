a1a03fd9c50060999cb6a11f2e6461ee278cebdb
Damian Robles <d.maxirobles@gmail.com>
2021-06-17 22:09:45 -0300

    se crea la carpeta web en el proyecto
    contiene subcarpetas como las de vistas, css, imagenes, tambien la base de datos y las paginas de plantilla.html e index.html

---
+++ web/scriptSQL/distribuidoraDB.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 5.7.15, for Win64 (x86_64)
+--
+-- Host: localhost    Database: distribuidoraDB
+-- ------------------------------------------------------
+-- Server version	5.7.15-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `distribuidoraDB`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `distribuidoraDB` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `distribuidoraDB`;
+
+--
+-- Table structure for table `pedidos`
+--
+
+DROP TABLE IF EXISTS `pedidos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `pedidos` (
+  `pedId` int(11) NOT NULL AUTO_INCREMENT,
+  `pedPeluqueria` varchar(50) DEFAULT NULL,
+  `pedProducto` varchar(20) DEFAULT NULL,
+  `pedFecha` date NOT NULL,
+  `pedCantidad` int(11) DEFAULT NULL,
+  PRIMARY KEY (`pedId`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pedidos`
+--
+
+LOCK TABLES `pedidos` WRITE;
+/*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
+INSERT INTO `pedidos` VALUES (1,'Claritos','shampoo','2016-11-03',2),(2,'AyC','alisador','2016-10-28',5);
+/*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-11-06 10:21:10
